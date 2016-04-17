0d5b0fd5dd33f8ba05cb7319970c199b2c1bb7fc
Dean _Gaffney <gaffmasterflex123@gmail.com>
2016-04-17 22:42:36 +0100

    All queries and view done

--- database_script.sql
+++ database_script.sql
@@ -164,7 +164,7 @@ VALUES((SELECT MAX(programmer_id) FROM Programmers) + 1,'Steve','Austin','01-JAN
 INSERT INTO Programmers
 VALUES((SELECT MAX(programmer_id) FROM Programmers) + 1,'Philip','Meagher','20-MAR-1992','10-NOV-2014','PhillyMeagher',
 	(SELECT (project_id) FROM Projects WHERE project_name = 'Kevins Crow'));
-
+--------------------------------------------------------------------------------------------------------------------------- UPDATE PROJECTS TABLE
 --****REMEMBER TO UPDATE PROJECTS NUM OF PROGRAMMERS ACCORDING TO THESE INSERTS*******
 
 UPDATE Projects
@@ -191,11 +191,11 @@ UPDATE Projects
 SET project_programmers = (SELECT COUNT(*) FROM Programmers 
 	WHERE project_id = (SELECT (project_id) FROM Projects WHERE project_name = 'X and O'))
 WHERE project_name = 'X and O';
-
+---------------------------------------------------------------------------------------------------------------------------DROP PROGRAMMERS TABLE (DEBUG)
 --Drop programmers table
 DROP TABLE Programmers;
 
----------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------- CREATE COMMITS TABLE
 
 --Create Commits Table
 CREATE TABLE Commits
@@ -210,7 +210,7 @@ CREATE TABLE Commits
 	CHECK(commit_number > 0)
 );
 
-
+--------------------------------------------------------------------------------------------------------------------------- INSERT INTO COMMITS TABLE
 --Populate Commits Table
 INSERT INTO Commits
 VALUES (1,'Added multiple recipients to email sending.',0,(SELECT (repo_id) FROM Repositories WHERE repo_name = 'kevins_crow'),
@@ -229,11 +229,11 @@ VALUES ((SELECT MAX(commit_number) FROM Commits) + 1,'Text file is now being com
 		SYSDATE);
 
 --****** CHECK AND MAKE SURE NOTHING NEEDS TO BE ALTERED iN OTHER TABLES BECAUSE OF THESE ADDITIONS********
-
+---------------------------------------------------------------------------------------------------------------------------DROP COMMITS TABLE (DEBUG)
 --Drop commits table
 DROP TABLE Commits;
 
----------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------- CREATE PUSHES TABLE
 CREATE TABLE Pushes
 (
 	push_id INT NOT NULL,
@@ -244,7 +244,7 @@ CREATE TABLE Pushes
 	CONSTRAINT fk_rep_id FOREIGN KEY (repo_id) REFERENCES Repositories(repo_id),
 	CHECK(push_id > 0)
 );
-
+--------------------------------------------------------------------------------------------------------------------------- INSERT INTO PUSHES TABLE
 --Populate Push Table
 INSERT INTO Pushes
 VALUES(1,SYSDATE,(SELECT (repo_id) FROM Repositories WHERE repo_name = 'kevins_crow'),
@@ -262,7 +262,7 @@ INSERT INTO Pushes
 VALUES((SELECT MAX(push_id) FROM Pushes) + 1,SYSDATE,(SELECT (repo_id) FROM Repositories WHERE repo_name = 'x_and_o'),
 	(SELECT (repo_username) FROM Programmers WHERE programmer_id = 4));
 
-
+--------------------------------------------------------------------------------------------------------------------------- UPDATE REPOSITORIES TABLE
 -- *****UPDATE PUSHES TO REPO HERE*******
 UPDATE Repositories
 SET pushes_made = (SELECT COUNT(*) FROM Pushes 
@@ -284,11 +284,11 @@ SET pushes_made = (SELECT COUNT(*) FROM Pushes
 	WHERE repo_id = (SELECT (repo_id) FROM Repositories WHERE repo_name = 'huffman_coding'))
 WHERE repo_name = 'huffman_coding';
 
-
+---------------------------------------------------------------------------------------------------------------------------DROP PUSHES TABLE (DEBUG)
 --Drop Push Table
 DROP TABLE Pushs;
 
----------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------- CREATE TABLE PULLS
 CREATE TABLE Pulls
 (
 	pull_id INT NOT NULL,
@@ -299,6 +299,7 @@ CREATE TABLE Pulls
 	CONSTRAINT fk_repos_id FOREIGN KEY (repo_id) REFERENCES Repositories(repo_id),
 	CHECK(pull_id > 0)
 );
+--------------------------------------------------------------------------------------------------------------------------- INSERT INTO PULLS TABLE
 
 INSERT INTO Pulls
 VALUES(1,(SELECT (repo_id) FROM Repositories WHERE repo_name = 'kevins_crow'),SYSDATE,
@@ -320,6 +321,7 @@ INSERT INTO Pulls
 VALUES((SELECT MAX(pull_id) FROM Pulls) + 1,(SELECT (repo_id) FROM Repositories WHERE repo_name = 'huffman_coding'),SYSDATE,
 	  (SELECT (repo_username) FROM Programmers WHERE programmer_id = 1));
 
+--------------------------------------------------------------------------------------------------------------------------- UPDATE REPOSITORIES TABLES
 -- *****UPDATE PUlls FROM REPO HERE*******
 UPDATE Repositories
 SET pulls_made = (SELECT COUNT(*) FROM Pulls 
@@ -336,10 +338,10 @@ SET pulls_made = (SELECT COUNT(*) FROM Pulls
 	WHERE repo_id = (SELECT (repo_id) FROM Repositories WHERE repo_name = 'x_and_o'))
 WHERE repo_name = 'x_and_o';
 
-
+---------------------------------------------------------------------------------------------------------------------------DROP PULLS TABLE (DEBUG)
 --Delete Pulls Table
 DROP TABLE Pulls;
----------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------- CREATE TABLE FILES
 
 --Create Files table
 CREATE TABLE Files
@@ -359,6 +361,8 @@ CREATE TABLE Files
 	CONSTRAINT fk_commit_number FOREIGN KEY (commit_number) REFERENCES Commits(commit_number),
 	CHECK(completed = 'Y' OR completed = 'N')
 );
+
+--------------------------------------------------------------------------------------------------------------------------- INSERT INTO FILES
 
 INSERT INTO Files
 VALUES('HuffmanCodingTree.java',SYSDATE,SYSDATE,'2kb',87,
@@ -384,6 +388,197 @@ VALUES('kevins_crow.py',SYSDATE,SYSDATE,'2kb',25,
 		 'Y','Emails pictures of crows to kevin everyday','Able to send email to serveral recipients',
 		  NULL);
 
-	
+--------------------------------------------------------------------------------------------------------------------------- DROP FILES TABLE(DEBUG)
 DROP TABLE Files;
+--------------------------------------------------------------------------------------------------------------------------- CREATE ALIASES VIEWS FOR TABLES
+CREATE OR REPLACE VIEW view_hosting_services_table							---->Hosting Service Table with Aliases
+("Name","URL","No. of Repositories","Sign up Date")
+AS SELECT service_name,service_url,num_of_repos,sign_up_date
+FROM Hosting_Services;
 
+SELECT * FROM view_hosting_services_table;
+
+CREATE OR REPLACE VIEW view_repositories_table
+("ID","Name","Branch Name","Size","No. of files","README","Up to date",		---->Repositories View Table with Aliases.
+	"Last commit time","Pushes","Pulls","Repository URL","Serivce URL")
+AS SELECT repo_id,repo_name,branch_name,repo_size,num_of_files,
+		  has_readme,up_to_date,last_commit_time,pushes_made,
+		  pulls_made,repo_url,service_url
+FROM Repositories;
+
+SELECT * FROM view_repositories_table;
+
+CREATE OR REPLACE VIEW view_projects_table
+("ID","Name","Start Date","Dead Line","No. of Programmers",
+	"Programming Language","IDE","Brief","Has Repository","Repository ID") 	---->Projects View Table with Aliases.
+AS SELECT project_id,project_name,project_start_date,
+		  project_dead_line,project_programmers,
+		  project_language,project_ide,project_brief,
+		  has_repo,repo_id
+FROM Projects;
+
+SELECT * FROM view_projects_table;
+
+CREATE OR REPLACE VIEW view_programmers_table
+("ID","First Name","Last Name","Date of Birth","Hire Date",					---->Programmers View Table with Aliases.
+	"Repository Username",
+	"Active Project ID")
+AS SELECT programmer_id,programmer_first_name,programmer_last_name,
+		  programmer_dob,hire_date,repo_username,project_id
+FROM Programmers;
+
+SELECT * FROM view_programmers_table;
+
+CREATE OR REPLACE VIEW view_commits_table
+("Commit Number","Comment","No. Files","Repository ID","Commit Time")
+AS SELECT commit_number,commit_comment,num_of_files,repository_id,commit_time ---->Commits View Table with Aliases.
+FROM Commits;
+
+SELECT * FROM view_commits_table;
+
+CREATE OR REPLACE VIEW view_pushes_table
+("ID","Time of Push","Repository ID","Programmer Username")					  ---->Pushes View Table with Aliases.
+AS SELECT push_id,time_of_push,repo_id,programmer_username
+FROM Pushes;
+
+SELECT * FROM view_pushes_table;
+
+CREATE OR REPLACE VIEW view_pulls_table
+("ID","Repository ID","Time of Pull","Programmer Username")					  ---->Pulls View Table with Aliases.
+AS SELECT pull_id,repo_id,pull_time,programmer_username
+FROM Pulls;
+
+SELECT * FROM view_pulls_table;
+
+CREATE OR REPLACE VIEW view_files_table										 ---->Files View Table with Aliases.
+("Name","Creation Date","Last Modified","Size","No. of Lines",
+"Programmer ID","Completed","Brief","Note","Commit Number")
+AS SELECT file_name,creation_date,last_modified,file_size,
+		  num_of_lines,programmer_id,completed,brief,
+		  note,commit_number
+FROM Files;
+
+SELECT * FROM view_files_table;
+
+--------------------------------------------------------------------------------------------------------------------------- CREATE VIEWS FOR SPECIFICS
+--Create some a views for the programmers.
+
+--git specific view (READ ONLY ACCESS)
+CREATE OR REPLACE VIEW view_programmers_of_git_repo
+("ID","Name","Branch","Files","README.md",
+	"Up to Date","Pushes","Pulls","Repo URL","Service","Serive Url")
+AS SELECT r.repo_id,r.repo_name,r.branch_name,r.num_of_files,
+		  r.has_readme,r.up_to_date,r.pushes_made,r.pulls_made,
+		  r.repo_url,h.service_name,h.service_url
+ FROM Repositories r JOIN Hosting_Services h ON r.service_url = h.service_url
+ WHERE service_name = 'GitHub'
+ WITH READ ONLY;
+
+ SELECT * FROM view_programmers_of_git_repo;
+
+--bitbucket specific view (READ ONLY ACCESS)
+CREATE OR REPLACE VIEW view_programmers_of_bit_repo
+("ID","Name","Branch","Files","README.md",
+	"Up to Date","Pushes","Pulls","Repo URL","Service","Serive Url")
+AS SELECT r.repo_id,r.repo_name,r.branch_name,r.num_of_files,
+		  r.has_readme,r.up_to_date,r.pushes_made,r.pulls_made,
+		  r.repo_url,h.service_name,h.service_url
+ FROM Repositories r JOIN Hosting_Services h ON r.service_url = h.service_url
+ WHERE service_name = 'BitBucket'
+ WITH READ ONLY;
+
+  SELECT * FROM view_programmers_of_bit_repo;
+
+ --view for the most active repo (based of of pulls - READ ONLY ACCESS)
+ CREATE OR REPLACE VIEW view_most_active_repo
+ ("ID","Name","Pushes","Pulls","Repo URL","Serivce","Serivce URL")
+ AS SELECT r.repo_id,r.repo_name,r.pushes_made,r.pulls_made,
+ 		   r.repo_url,h.service_name,h.service_url
+ FROM Repositories r JOIN Hosting_Services h ON r.service_url = h.service_url
+ WHERE pulls_made = (SELECT MAX(pulls_made) FROM Repositories)
+ WITH READ ONLY;
+
+ SELECT * FROM view_most_active_repo;
+
+
+--------------------------------------------------------------------------------------------------------------------------- CREATE ROLE,GRANT AND REVOKE ROLE
+--Create user conor with select,update and delete privileges
+CREATE USER conor_corcoran
+IDENTIFIED BY conor123;
+
+--Create a role for people to update databases.
+CREATE ROLE updaters;											--->Create role
+
+GRANT SELECT,UPDATE,DELETE ON Programmers TO updaters;			----> Grant some priveleges to the role
+GRANT updaters TO conor_corcoran;								---->GRANT USER ROLE ACCESS
+
+--Revoke DELETE privilege.
+REVOKE DELETE ON Programmers FROM updaters;						---->REVOKE DELETE PRIVILEEGE FROM ROLE
+
+DROP ROLE updaters;												---->DROP THE ROLE
+--------------------------------------------------------------------------------------------------------------------------- 10 QUERIES
+--What programmers are currently working on a specific project.
+
+SELECT p.programmer_id AS "ID",p.programmer_first_name AS "First Name",													----> QUEREY 1.
+	   p.programmer_last_name AS "Last Name",pro.project_id AS "Project ID",
+	   pro.project_name AS "Project Name"
+FROM Programmers p JOIN Projects pro ON p.project_id = pro.project_id
+WHERE p.project_id = pro.project_id AND pro.project_name LIKE '%Kev%';
+
+--What programmer wrote a particular file. (uses regex to find files ending with .java or .py)
+SELECT p.programmer_id AS "ID",p.programmer_first_name AS "First Name",													----> QUEREY 2.
+	   p.programmer_last_name AS "Last Name",f.file_name AS "File Name"
+FROM Programmers p JOIN Files f ON p.programmer_id = f.programmer_id
+WHERE p.programmer_id = f.programmer_id AND REGEXP_LIKE (f.file_name,'\.(java|py)$');
+
+--How many repositories are set up																						----> QUEREY 3.
+SELECT SUM(num_of_repos) AS "No. of Repositories" FROM Hosting_Services;
+
+
+--The average lines of code per file.
+SELECT AVG(num_of_lines) AS "Avearage Lines of Code" FROM Files;														----> QUEREY 4
+
+
+--What language is the project being coded in.
+SELECT '----'  || project_language || '----' AS "Language"																----> QUEREY 5.
+FROM Projects
+WHERE project_name = 'Kevins Crow';
+
+--All programmers that are registered to a repository hosting service.
+SELECT programmer_id AS "ID",																							----> QUEREY 6.
+	   programmer_first_name ||' ' ||programmer_last_name AS "Name",
+	   repo_username AS "Repository Username"
+FROM Programmers;
+
+
+--List all repositories which are up to up to Date                														----> QUEREY 7.
+SELECT 'ID--->' || repo_id ||  '    NAME--->' || repo_name
+AS "Not Updated Repositories"
+FROM Repositories
+WHERE up_to_date = 'Y';
+
+--See if any dead lines for projects are within the current month.														----> QUEREY 8.
+SELECT project_id AS "ID", project_name AS "Name", project_dead_line
+AS "DeadLine"
+FROM Projects
+WHERE EXTRACT(month FROM project_dead_line) = EXTRACT(month FROM SYSDATE);
+
+
+
+--The last time a repository had a commit made to it.																	----> QUEREY 9.
+SELECT repo_id AS "ID",repo_name AS "Name",
+	   MAX(last_commit_time) AS "Last Commit" 
+FROM Repositories
+WHERE repo_name = 'kevins_crow'
+GROUP BY repo_id, repo_name
+ORDER BY repo_id;
+
+--What comment was given to a certain commit.																			---->QUEREY 10.
+SELECT commit_number AS "Commit Number",
+	   commit_comment AS "Comment"
+FROM Commits
+WHERE commit_number = 1;
+
+
+
+	
